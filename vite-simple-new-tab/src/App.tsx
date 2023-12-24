895b71b021876f073287b9f5ae27698db20282fe
Torrance Cui <torrancecui@Torrances-MacBook-Pro.local>
2023-12-23 20:40:30 -0800

    added gsap animations

--- vite-simple-new-tab/src/App.tsx
+++ vite-simple-new-tab/src/App.tsx
@@ -3,14 +3,30 @@ import ParentSize from '@visx/responsive/lib/components/ParentSize';
 import Streamgraph from './Streamgraph'
 import Clock from './Clock';
 
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { Power3 } from 'gsap';
+
+
 function App() {
+
+  const clock: any = useRef();
+  const streamgraph: any = useRef();
+
+  useGSAP(() => {
+    gsap.from(clock.current, { opacity: 0, ease: Power3.easeIn });
+    gsap.from(streamgraph.current, { opacity: 0, ease: Power3.easeIn, delay: 0.3 });
+  });
+
+
   return (
     <>
       <div className='App'>
-        <div className='Clock'>
+        <div className='Clock' ref={clock}>
           <Clock></Clock>
         </div>
-        <div className='Graph'>
+        <div className='Graph' ref={streamgraph}>
           <ParentSize>{({ width, height }) => <Streamgraph width={width} height={height} />}</ParentSize>
         </div>
       </div>
